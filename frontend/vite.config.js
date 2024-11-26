cc7caf2207d7bc97f32a5448ce4ab54578c0fc95
Danny <danishskh70@gmail.com>
2024-11-26 23:08:50 +0530

    Fix Not Found issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensures relative paths for assets and routes
   build: {
-    outDir: 'dist', // Default build directory
+    outDir: 'dist',
   },
 });
